ddd667105d216491e5dac0fc0c87353461d21ad3
thongcao2811 <166991236+thongcao2811@users.noreply.github.com>
2024-04-15 16:03:14 +0700

    delete

--- src/JavaBasic/ThongTin.java
+++
@@ -1,7 +0,0 @@
-package JavaBasic;
-
-public class ThongTin {
-
-    public static String userName = "CaoTienThong";
-    public static String passWord = "123456";
-}
--- src/TinhDaHinh/DemoGhiDe.java
+++
@@ -1,4 +0,0 @@
-package TinhDaHinh;
-
-public class DemoGhiDe {
-}
